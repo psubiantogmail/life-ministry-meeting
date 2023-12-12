84cda278972d3976df1e8f9d8e6061e9d27120a5
psubianto@gmail.com <psubianto@gmail.com>
2023-12-12 14:52:10 +0000

    Edited Form

--- client_code/Form/__init__.py
+++ client_code/Form/__init__.py
@@ -10,3 +10,10 @@ class Form(FormTemplate):
     # Any code you write here will run before the form opens.
     language_list = ['Indonesian', 'English']
     self.drop_down_language.items = language_list
+
+  def drop_down_language_change(self, **event_args):
+    """This method is called when an item is selected"""
+    if self.drop_down_language.selected_value == 'Indonesian':
+      self.link_epub.url = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
+    else:
+      self.link_epub.url = "https://www.jw.org/en/library/jw-meeting-workbook/"
