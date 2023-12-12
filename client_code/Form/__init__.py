1c6d23a18d6ffb822c96e6ffcf6e8175629f5925
psubianto@gmail.com <psubianto@gmail.com>
2023-12-12 20:52:14 +0000

    Edited ServerModule and Form

--- client_code/Form/__init__.py
+++ client_code/Form/__init__.py
@@ -10,10 +10,19 @@ class Form(FormTemplate):
     # Any code you write here will run before the form opens.
     language_list = ['Indonesian', 'English']
     self.drop_down_language.items = language_list
+    self.link_epub.url = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
+    self.link_epub.text = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
+
 
   def drop_down_language_change(self, **event_args):
     """This method is called when an item is selected"""
     if self.drop_down_language.selected_value == 'Indonesian':
       self.link_epub.url = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
+      self.link_epub.text = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
     else:
       self.link_epub.url = "https://www.jw.org/en/library/jw-meeting-workbook/"
+      self.link_epub.text = "https://www.jw.org/en/library/jw-meeting-workbook/"
+
+  def link_jworg_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -1,11 +1,9 @@
 import anvil.server
 
 @anvil.server.callable
-def get_epub(language):
-  site = 'https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/'
-  
-  if language == 'English':
-    site = 'https://www.jw.org/en/library/jw-meeting-workbook/'
+def get_epub(site):
+  if not site:
+    site = "https://www.jw.org/id/perpustakaan/jw-lembar-pelajaran/"
 
   
 
